a6d33a5c3fe63e19581d1f718054041183db53f3
Pedro Souza <pedronicholas.souza@gmail.com>
2015-01-21 17:24:45 -0200

    adding files to extThree20CSStyle

--- Pod/Classes/extThree20CSSStyle/Headers/TTDefaultCSSStyleSheet.h
+++ Pod/Classes/extThree20CSSStyle/Headers/TTDefaultCSSStyleSheet.h
@@ -17,21 +17,52 @@
 #import "Three20Style/TTDefaultStyleSheet.h"
 
 @class TTCSSStyleSheet;
+@class TTCSSRuleSet;
+@protocol TTCSSApplyProtocol;
 
-#define TTCSSBGCOLOR(selector)  [[TTDefaultCSSStyleSheet globalCSSStyleSheet] \
-                                  backgroundColorForCSSSelector:selector]
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 @interface TTDefaultCSSStyleSheet : TTDefaultStyleSheet {
 @private
   TTCSSStyleSheet* _styleSheet;
+
+  // Maintain an control of CSS Files already loaded and cached.
+  NSMutableSet*				_cachedCssFiles;
+
 }
 
 @property (nonatomic, readonly) TTCSSStyleSheet* styleSheet;
 
+/**
+ * Load an CSS Style Sheet from disk and cache his data.
+ * If the file is already cached no data will be loaded again,
+ * if you need to reload the file use addStyleSheetFromDisk:ignoreCache:
+ */
 - (BOOL)addStyleSheetFromDisk:(NSString*)filename;
 
-- (UIColor*)backgroundColorForCSSSelector:(NSString*)cssSelector;
+/**
+ * Load an CSS Style Sheet from disk and cache his data.
+ * @param cache YES will ignore if is already cached and reload the data if needed.
+ */
+- (BOOL)addStyleSheetFromDisk:(NSString*)filename ignoreCache:(BOOL)cache;
 
 + (TTDefaultCSSStyleSheet*)globalCSSStyleSheet;
+
+/**
+ * CSS Rule Set.
+ */
+-(TTCSSRuleSet*)css:(NSString*)selectorName;
+
+/**
+ * CSS Rule Set, also accept an specific state.
+ */
+-(TTCSSRuleSet*)css:(NSString*)selectorName forState:(UIControlState)state;
+
+/**
+ * Apply the rules for the specified selector to the informed object. This object
+ * must conform with the TTCSSApplyProtocol to read and properly apply the CSS rules.
+ */
+-(void)applyCssFromSelector:(NSString*)selectorName toObject:(id<TTCSSApplyProtocol>)anObject;
 
 @end
